07431b21896bcacc73c0f4698aa40e6aa8f72e99
cfd <2909047212@qq.com>
2024-01-14 10:28:20 +0800

    sql file

--- docs/sql/dousheng.sql
+++ docs/sql/dousheng.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `douyin` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `douyin`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: douyin
@@ -239,4 +241,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-01-13 21:13:14
+-- Dump completed on 2024-01-14 10:27:54
