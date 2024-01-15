ac43a6e585ccd75ba062f9e522ad05cb342928a3
gaoxiaozhi <2933756974@qq.com>
2024-01-15 16:54:37 +0800

    video.pb修改

--- video_service/handler/comment.go
+++ video_service/handler/comment.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	"video_service/model"
 	video_pb "video_service/server"
 )
 
@@ -9,6 +10,16 @@ func (v *VideoService) CommentAction(ctx *gin.Context, req *video_pb.CommentActi
 	resp = new(video_pb.CommentActionResponse)
 	// 判断是删除评论还是增加评论
 
-	comment :=
+	comment := &model.Comment{
+		UserId:  req.UserId,
+		VideoId: req.VideoId,
+		Content: req.CommentText,
+	}
+
+	if req.ActionType == 1 { // 发布评论
+
+	} else { // 删除评论
+
+	}
 	return resp, nil
 }
--- video_service/model/comment.go
+++ video_service/model/comment.go
@@ -3,12 +3,11 @@ package model
 import "time"
 
 type Comment struct {
-	Id            int64 `gorm:"primaryKey"`
-	UserId        int64
-	VideoId       int64
-	Content       string `gorm:"default:(-)"` // 评论内容
-	CreatedAt     time.Time
-	UpdatedAt     time.Time
-	DeletedAt     time.Time
-	CommentStatus bool `gorm:"default:(-)"`
+	Id        int64 `gorm:"primaryKey"`
+	UserId    int64
+	VideoId   int64
+	Content   string `gorm:"default:(-)"` // 评论内容
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt time.Time
 }
