1b6d6a634d66f410c7721d205e7f93829ccdee34
cfd <2909047212@qq.com>
2024-01-17 14:38:29 +0800

    修改sql文件

--- docs/sql/dousheng.sql
+++ docs/sql/dousheng.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `douyin` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `douyin`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: douyin
@@ -16,8 +14,6 @@ USE `douyin`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
 
 --
 -- Table structure for table `comments`
@@ -111,38 +107,6 @@ LOCK TABLES `messages` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `posts`
---
-
-DROP TABLE IF EXISTS `posts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `posts` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `created_at` datetime(3) DEFAULT NULL,
-  `updated_at` datetime(3) DEFAULT NULL,
-  `deleted_at` datetime(3) DEFAULT NULL,
-  `user_id` bigint unsigned DEFAULT NULL,
-  `video_id` bigint unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idx_posts_deleted_at` (`deleted_at`),
-  KEY `fk_users_posts` (`user_id`),
-  KEY `fk_posts_videos1_idx` (`video_id`),
-  CONSTRAINT `fk_posts_videos1` FOREIGN KEY (`video_id`) REFERENCES `videos` (`id`),
-  CONSTRAINT `fk_users_posts` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `posts`
---
-
-LOCK TABLES `posts` WRITE;
-/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `user_relations`
 --
 
@@ -220,8 +184,11 @@ CREATE TABLE `videos` (
   `favorite_count` bigint DEFAULT NULL,
   `comment_count` bigint DEFAULT NULL,
   `title` varchar(256) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `auth_id` bigint unsigned NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `idx_videos_deleted_at` (`deleted_at`)
+  KEY `idx_videos_deleted_at` (`deleted_at`),
+  KEY `fk_videos_users1_idx` (`auth_id`),
+  CONSTRAINT `fk_videos_users1` FOREIGN KEY (`auth_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -243,4 +210,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-01-14 10:27:54
+-- Dump completed on 2024-01-17 14:37:47
