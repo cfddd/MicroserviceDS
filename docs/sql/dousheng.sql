e2f718df726a9e1f5ae1c7f405ff367a47c04adc
cfd <2909047212@qq.com>
2024-01-17 16:57:29 +0800

    修改etcd配置为云服务器上固定配置

--- docs/sql/dousheng.sql
+++ docs/sql/dousheng.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `douyin` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `douyin`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: douyin
@@ -210,4 +212,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-01-17 14:37:47
+-- Dump completed on 2024-01-17 16:19:51
