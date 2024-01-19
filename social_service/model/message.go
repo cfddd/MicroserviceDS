cbf6d5c361c8684c7cd5923ece13557ff698eddc
gaoxiaozhi <2933756974@qq.com>
2024-01-19 16:22:59 +0800

    用户聊天处修改

--- social_service/model/message.go
+++ social_service/model/message.go
@@ -7,11 +7,11 @@ import (
 )
 
 type Messages struct {
-	Id        int64 `gorm:"primary_key"`
-	UserId    int64
-	ToUserId  int64
-	Message   string
-	CreatedAt time.Time
+	Id         int64     `json:"id" gorm:"primary_key"`
+	UserId     int64     `json:"user_id" gorm:"foreignKey:UserId;column:user_id"` // 指定外键关系
+	FollowToID int64     `json:"follow_to_id" gorm:"column:follow_to_id"`
+	Content    string    `json:"content" gorm:"column:content"`
+	CreatedAt  time.Time `json:"created_at" gorm:"column:created_at"`
 }
 
 type MessageModel struct {
@@ -38,6 +38,7 @@ func (m *MessageModel) PostMessage(message Messages) error {
 }
 
 func (m *MessageModel) GetMessage(uId, tId, preMsgTime int64, messages *[]Messages) error {
-	err := DB.Model(&Messages{}).Where("UserId + ToUserId = ? and CreatedAt > ? ", uId+tId, preMsgTime).Find(messages).Error
+	time := time.Unix(preMsgTime, 0)
+	err := DB.Model(&Messages{}).Where("user_id + follow_to_id = ? and created_at > ? ", uId+tId, time).Find(messages).Error
 	return err
 }
