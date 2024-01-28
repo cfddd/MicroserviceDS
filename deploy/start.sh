bff1848950ce0caff41f0dba3069e788f63c8455
cfd <2909047212@qq.com>
2024-01-29 00:04:35 +0800

    => ERROR [builder 10/10] COPY ./configs/ /build/configs/                                                                                                          0.0s ------  > [builder 10/10] COPY ./configs/ /build/configs/: ------ Dockerfile:21 --------------------   19 |     RUN go build -ldflags="-s -w" -o /app/main main.go   20 |     # 复制本地的 configs 文件夹到容器内部的 /app 目录   21 | >>> COPY ./configs/ /build/configs/   22 |   23 |     FROM scratch -------------------- ERROR: failed to solve: failed to compute cache key: failed to calculate checksum of ref 5a3819f5-47bf-49ef-8267-a6f07b1e6172::l7xshjh8uci35pmd2912ln43u: "/configs": not found2

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,11 +1,11 @@
 docker build -t router_service_image ./router_service
-docker run --name router_service_container --network host -d router_service_image
+docker run -v ./router_service/configs:/build/configs --name router_service_container --network host -d router_service_image
 
 docker build -t social_service_image ./social_service
-docker run --name social_service_container --network host -d social_service_image
+docker run -v ./social_service/configs:/build/configs --name social_service_container --network host -d social_service_image
 
 docker build -t user_service_image ./user_service
-docker run --name user_service_container --network host -d user_service_image
+docker run -v ./user_service/configs:/build/configs --name user_service_container --network host -d user_service_image
 
 docker build -t video_service_image ./video_service
-docker run  --name video_service_container --network host -d video_service_image
+docker run  -v ./video_service/configs:/build/configs --name video_service_container --network host -d video_service_image
