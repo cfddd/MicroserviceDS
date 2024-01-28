5f0241c05a7a363e7aef36552585c1d721184d0f
cfd <2909047212@qq.com>
2024-01-29 00:34:31 +0800

    config and configs

--- deploy/start.sh
+++ deploy/start.sh
@@ -3,10 +3,10 @@ docker build -t router_service_image ./router_service
 docker run -v ./router_service/configs:/build/configs --name router_service_container --network host -d router_service_image
 
 docker build -t social_service_image ./social_service
-docker run -v ./social_service/configs:/build/configs --name social_service_container --network host -d social_service_image
+docker run -v ./social_service/config:/build/config --name social_service_container --network host -d social_service_image
 
 docker build -t user_service_image ./user_service
-docker run -v ./user_service/configs:/build/configs --name user_service_container --network host -d user_service_image
+docker run -v ./user_service/config:/build/config --name user_service_container --network host -d user_service_image
 
 docker build -t video_service_image ./video_service
-docker run  -v ./video_service/configs:/build/configs --name video_service_container --network host -d video_service_image
+docker run  -v ./video_service/config:/build/config --name video_service_container --network host -d video_service_image
