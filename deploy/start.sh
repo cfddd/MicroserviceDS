9e364e49811123a8a197672697470d1d801d38ad
cfd <2909047212@qq.com>
2024-01-28 23:27:28 +0800

    [2024-01-28 23:01:46] [info] 日志模块初始化成功 panic: Config File "config" Not Found in "[/build/configs]"
    goroutine 1 [running]:
    router_service/configs.InitConfig()
    	/build/configs/config.go:22 +0x13d
    main.main()
    	/build/main.go:14 +0x25
    修改了虚拟机上docker运行的问题

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,11 +1,11 @@
 docker build -t router_service_image ./router_service
-docker run -d --network host --name router_service_container router_service_image
+docker run -d --network host --name router_service_container
 
 docker build -t social_service_image ./social_service
-docker run -d --network host --name social_service_container social_service_image
+docker run -d --network host --name social_service_container
 
 docker build -t user_service_image ./user_service
-docker run -d --network host --name user_service_container user_service_image
+docker run -d --network host --name user_service_container
 
 docker build -t video_service_image ./video_service
-docker run -d --network host --name video_service_container video_service_image
+docker run -d --network host --name video_service_container
