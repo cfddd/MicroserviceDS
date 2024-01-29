3f22f0eb3ff5382b0fc3a563029deb7c7f87e815
cfd <2909047212@qq.com>
2024-01-29 17:14:19 +0800

    ffmpeg环境变量问题

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,6 +1,6 @@
 # 在MicroserviceDS项目根路径下运行docker命令
 docker build -t router_service_image ./router_service
-docker run -v ./router_service/configs:/build/configs --name router_service_container --network host -d router_service_image
+docker run -v ./router_service/configs:/build/configs -v /usr/local/ffmpeg:/usr/local/ffmpeg --name router_service_container --network host -d router_service_image
 
 docker build -t social_service_image ./social_service
 docker run -v ./social_service/config:/build/config --name social_service_container --network host -d social_service_image
@@ -11,4 +11,4 @@ docker run -v ./user_service/config:/build/config --name user_service_container 
 docker build -t video_service_image ./video_service
 docker run  -v ./video_service/config:/build/config --name video_service_container --network host -d video_service_image
 
-# 还需要配置ffmpeg，参考https://zhuanlan.zhihu.com/p/347780238
+# 还需要配置ffmpeg，参考https://cloud.tencent.com/developer/article/1711770
